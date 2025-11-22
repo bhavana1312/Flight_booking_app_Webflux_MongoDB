9f7b13762249e1c22445ce216edb9f475044e145
bhavana1312 <bhavana13sree@gmail.com>
2025-11-22 18:20:20 +0530

    update pnr generation logic

--- src/main/java/com/flightapp/util/PnrGenerator.java
+++ src/main/java/com/flightapp/util/PnrGenerator.java
@@ -1,15 +1,12 @@
 package com.flightapp.util;
 
-import java.security.SecureRandom;
+import java.util.UUID;
 
 public class PnrGenerator {
-	private static final String CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-	private static final SecureRandom rnd = new SecureRandom();
+	private PnrGenerator() {
+	}
 
-	public static String gen() {
-		StringBuilder sb = new StringBuilder(6);
-		for (int i = 0; i < 6; i++)
-			sb.append(CHARS.charAt(rnd.nextInt(CHARS.length())));
-		return sb.toString();
+	public static String generate() {
+		return UUID.randomUUID().toString().replace("-", "").substring(0, 10).toUpperCase();
 	}
 }
