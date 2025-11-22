bbc1e561ce417864523ab02a2a070f0dc12c7ed5
bhavana1312 <bhavana13sree@gmail.com>
2025-11-22 18:47:58 +0530

    add cancel endpoint

--- src/main/java/com/flightapp/controller/FlightController.java
+++ src/main/java/com/flightapp/controller/FlightController.java
@@ -1,4 +1,5 @@
 package com.flightapp.controller;
+
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -13,25 +14,34 @@ import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/flight")
-public class FlightController{
- private final InventoryRepository invRepo;
- private final BookingService bookingService;
- public FlightController(InventoryRepository invRepo,BookingService bookingService){this.invRepo=invRepo;this.bookingService=bookingService;}
+public class FlightController {
+	private final InventoryRepository invRepo;
+	private final BookingService bookingService;
 
- @PostMapping("/search")
- public Flux<Inventory> search(@RequestBody SearchRequest req){
-  LocalDateTime start=req.date.atStartOfDay();
-  LocalDateTime end=start.plusDays(1);
-  return invRepo.findByFromAndToAndDepartureBetween(req.from,req.to,start,end);
-}
+	public FlightController(InventoryRepository invRepo, BookingService bookingService) {
+		this.invRepo = invRepo;
+		this.bookingService = bookingService;
+	}
 
- @PostMapping("/booking/{flightid}")
- public Mono<Booking> book(@PathVariable("flightid") String flightid,@RequestBody BookingRequest req){
-  return bookingService.book(flightid,req);
- }
+	@PostMapping("/search")
+	public Flux<Inventory> search(@RequestBody SearchRequest req) {
+		LocalDateTime start = req.date.atStartOfDay();
+		LocalDateTime end = start.plusDays(1);
+		return invRepo.findByFromAndToAndDepartureBetween(req.from, req.to, start, end);
+	}
 
- @GetMapping("/ticket/{pnr}")
- public Mono<Booking> getByPnr(@PathVariable("pnr") String pnr){
-  return bookingService.findByPnr(pnr);
- }
+	@PostMapping("/booking/{flightid}")
+	public Mono<Booking> book(@PathVariable("flightid") String flightid, @RequestBody BookingRequest req) {
+		return bookingService.book(flightid, req);
+	}
+
+	@GetMapping("/ticket/{pnr}")
+	public Mono<Booking> getByPnr(@PathVariable("pnr") String pnr) {
+		return bookingService.findByPnr(pnr);
+	}
+
+	@DeleteMapping("/booking/cancel/{pnr}")
+	public Mono<Booking> cancel(@PathVariable("pnr") String pnr) {
+		return bookingService.cancelTicket(pnr);
+	}
 }
