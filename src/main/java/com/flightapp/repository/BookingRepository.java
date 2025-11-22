3aeb5079506260949b407b54a7b993daa7a50920
bhavana1312 <bhavana13sree@gmail.com>
2025-11-22 18:17:19 +0530

    update application properties

--- src/main/java/com/flightapp/repository/BookingRepository.java
+++ src/main/java/com/flightapp/repository/BookingRepository.java
@@ -1,9 +1,12 @@
 package com.flightapp.repository;
+
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import com.flightapp.model.Booking;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-public interface BookingRepository extends ReactiveMongoRepository<Booking,String>{
- Flux<Booking> findByEmail(String email);
- Mono<Booking> findByPnr(String pnr);
+
+public interface BookingRepository extends ReactiveMongoRepository<Booking, String> {
+	Flux<Booking> findByEmail(String email);
+
+	Mono<Booking> findByPnr(String pnr);
 }
