84282f9bcfd4d480523e0c58655bc613317671a1
bhavana1312 <bhavana13sree@gmail.com>
2025-11-22 18:22:27 +0530

    add booking service

---
+++ src/main/java/com/flightapp/service/BookingService.java
@@ -0,0 +1,11 @@
+package com.flightapp.service;
+
+import com.flightapp.dto.BookingRequest;
+import com.flightapp.model.Booking;
+import reactor.core.publisher.Mono;
+
+public interface BookingService {
+	Mono<Booking> book(String flightId, BookingRequest req);
+
+	Mono<Booking> findByPnr(String pnr);
+}
